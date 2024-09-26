513691170abc4d8a3d70be9fd42162511e6bb64d
Key19991232 <98572069+Key19991232@users.noreply.github.com>
2024-09-27 00:09:54 +0800

    Add files via upload

---
+++ root_ltm/main.tf
@@ -0,0 +1,8 @@
+
+
+
+
+
+  module "pip" {
+    source                   = "github.com/GTI-CTU/hkjc-jenkins-tfdemo/mod_ltm"
+  }
---
+++ root_ltm/provider.tf
@@ -0,0 +1,19 @@
+#########################################################################################
+#------------------------- Tenant - Hong Kong Jockey Club (SAT) ------------------------#
+#----------------------------- Subscription - hkjc-sat-gtm -----------------------------#
+#########################################################################################
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=4.2.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+    subscription_id = local.subscription_id          
+}
+
